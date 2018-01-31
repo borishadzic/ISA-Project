4090cd5269dc700d224bfb00b8c50eb7bdf345d6
Artemoire <dejan.tot@gmail.com>
2018-01-31 20:22:41 +0100

    Added Theater class

---
+++ ISofA.DAL/Models/Theater.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISofA.DAL.Models
+{
+    public class Theater
+    {
+        public int TheaterId { get; set; }
+    }
+}
