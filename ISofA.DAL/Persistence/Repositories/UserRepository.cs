398c58cc59d25f584e78d8fdb8e83f29a24e1ad5
Artemoire <dejan.tot@gmail.com>
2018-02-01 18:13:14 +0100

    Added Ninject, implemented repositories for DI test

---
+++ ISofA.DAL/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using ISofA.DAL.Core.Domain;
+using ISofA.DAL.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISofA.DAL.Persistence.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public IEnumerable<ISofAUser> Get()
+        {
+            using(var context = new ISofADbContext())
+            {
+                return context.Users.ToList();
+            }
+        }
+    }
+}
---
+++ ISofA.WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using ISofA.DAL.Core.Domain;
+using ISofA.DAL.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ISofA.WebAPI.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;        
+        }
+
+        public IEnumerable<ISofAUser> Get()
+        {
+            return _userRepository.Get();
+        }
+    }
+}
